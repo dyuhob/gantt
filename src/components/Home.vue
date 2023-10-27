d95ca9080b22bbf54a2719467df26b6a4dbc4826
김도협 <dyuhob8056@computermate.co.kr>
2023-10-27 14:19:01 +0900

    again

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,35 +1,52 @@
 <template>
   <div>
-    
+    <div ref="yourElementRef"></div>
+    <div><button @click="chart">chartMaker</button></div>
   </div>
 </template>
 
 <script>
- import {
+import {
     MomentSvelteGanttDateAdapter,
     SvelteGantt,
     SvelteGanttDependencies,
     SvelteGanttTable,
-    SvelteGanttExternal
   } from 'svelte-gantt'
 
-    export default {
-        name: 'Home',
-        components: {
-            MomentSvelteGanttDateAdapter,
-      SvelteGanttExternal,
-      SvelteGantt,
-      SvelteGanttDependencies,
-      SvelteGanttTable,
-        },
-        data() {
-            return {
-                
-            };
-        },
+export default {
+  name: 'Home',
+  data() {
+    return {
+      props: {
+        dateAdapter: new MomentSvelteGanttDateAdapter(moment),
+        rows: [{ id: 1, label: 'abc' }],
+        tasks: [{ id: 1, resourceId: 1, from: new Date('2023-10-27').getTime(), to: new Date('2023-10-28').getTime(), label: 'dddd' }],
+        columnOffset: 1,
+        columnUnit: 'year',
+        magnetOffset: 1,
+        rowHeight: 52,
+        rowPadding: 6,
+        headers: [{ unit: 'day', format: 'MM-DD', sticky: true }],
+        fitWidth: true,
+        minWidth: 1000,
+        from: new Date('2023-10-26').getTime(),
+        to: new Date('2023-10-28').getTime(),
+        tableHeaders: [{ title: '', property: 'label', width: 140, type: 'tree' }],
+        tableWidth: 180,
+        ganttTableModules: [SvelteGanttTable],
+        ganttBodyModules: [SvelteGanttDependencies],
+        resizeHandleWidth: -100,
+      },
     };
-</script>
+  },
+  methods: {
+    chart() {
+      new SvelteGantt(this.$refs.yourElementRef, this.props);
+    }
+  }
+};
 
-<style>
-  
-</style>
+
+
+</script>
+<style></style>
